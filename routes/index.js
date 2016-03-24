9affd24fd8a6c955453f73373abff23118ae0879
abhijit kini <mail.abhijitkini@gmail.com>
2016-03-24 12:28:52 -0700

    Updates to have Shippable deploy changes to Elastic Beanstalk

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Shippable automating deployment to your Elastic Beanstalk' });
+  res.render('index', { title: 'Shippable' });
 });
 
 module.exports = router;
